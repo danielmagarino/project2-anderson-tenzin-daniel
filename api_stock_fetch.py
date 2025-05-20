ffe53add85b522c35b9b7a35d98097b97b7dc19e
Tenzin Choedhen <tenzinchoedhen@Mac.lan>
2025-05-20 02:10:11 -0400

    working on it

--- api_stock_fetch.py
+++ api_stock_fetch.py
@@ -4,17 +4,15 @@ import time
 
 
 DF1= pd.read_csv("nasdaq_df1.csv")
-
 url="https://alpha-vantage.p.rapidapi.com/query"
 
 headers = {
-    "X-RapidAPI-Key": "ef5a3c33d3msh7776c931f35e89cp18e5ffjsnf36112350622",  #  Replace with your actual key
+    "X-RapidAPI-Key": "(own key)",  #  Replace with your own key
     "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"
 }
 
 
-results=[]
-
+results=[] 
 Tickers = DF1["Ticker"]
 
 
@@ -46,14 +44,20 @@ for ticker in Tickers:
 
             print(f" Data collected for {ticker}")
         else:
-            print(f"⚠️ No data for {ticker} - {data.get('Note', 'unknown error')}")
+            print(f" No data for {ticker}")
     except Exception as e:
         print(f" Error with {ticker}: {e}")
 
     time.sleep(12)
 
+if results:
+    DF2 = pd.DataFrame(results)
+    DF2.to_csv("nasdaq_df2.csv", index=False)
+    print(" File saved as nasdaq_df2.csv")
+else:
+    print(" No data to save.")
  
 
 
         
-    
+     
